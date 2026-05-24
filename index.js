7973b56a97b5cdd70f12a6de6d5af52bafa01a26
Numsik <honzik.klusek.myto@gmail.com>
2026-05-24 11:29:20 +0200

    na zacatkos

---
+++ index.js
@@ -0,0 +1,22 @@
+const questions = [
+    {questions: "Hlavni mesto Ceska ?",
+        answer: ["Praha", "Berlin", "Bratislava"],
+        correct: 1
+    },
+    {questions: "Hlavni mesto SLovenska ?",
+        answer: ["Praha", "Berlin", "Bratislava"],
+        correct: 3
+    },
+    {questions: "Hlavni mesto Polska ?",
+        answer: ["Praha", "Varšava", "Bratislava"],
+        correct: 2
+    },
+    {questions: "Hlavni mesto Nemecka ?",
+        answer: ["Praha", "Berlin", "Bratislava"],
+        correct: 2
+    },
+    {questions: "Hlavni mesto Rakouska ?",
+        answer: ["Viden", "Berlin", "Bratislava"],
+        correct: 1
+    }
+]

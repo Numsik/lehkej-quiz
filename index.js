878c8c93a4830b8b4ac1a0f757297fba2cbe475b
Numsik <honzik.klusek.myto@gmail.com>
2026-05-24 20:59:59 +0200

    v4

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const questions = [
     {questions: "Hlavni mesto Ceska ?",
         answer: ["Praha", "Berlin", "Bratislava"],
-        correct: 1
+        correct: 0
     },
     {questions: "Hlavni mesto Slovenska ?",
         answer: ["Praha", "Berlin", "Bratislava"],
-        correct: 3
+        correct: 2
     },
     {questions: "Hlavni mesto Polska ?",
         answer: ["Praha", "Varšava", "Bratislava"],
-        correct: 2
+        correct: 1
     },
     {questions: "Hlavni mesto Nemecka ?",
         answer: ["Praha", "Berlin", "Bratislava"],
-        correct: 2
+        correct: 1
     },
     {questions: "Hlavni mesto Rakouska ?",
         answer: ["Viden", "Berlin", "Bratislava"],
-        correct: 1
+        correct: 0
     }
 ]
 
@@ -26,10 +26,32 @@ function showQuestion(index) {
 
   document.getElementById("question").innerHTML = q.questions
   document.getElementById("answers").innerHTML = q.answer
-    .map((answer, i) => `<button>${answer}</button>`).join("")
+    .map((answer, i) => `<button type="button" id="btn"  onclick="getAnotherQuestion(${index}); CorrectOrNot(${q.correct},${i})">${answer}</button>`).join("")
 }
 
 function getRandomNum(max){
     return Math.floor(Math.random() * max)
 }
 showQuestion(getRandomNum(questions.length))
+
+function getAnotherQuestion(index){
+    const next = index + 1
+    if (next <= questions.length - 1){
+        showQuestion(next)
+    }else{
+        console.log("no more questions")
+    }
+
+}
+
+let scorenum = 0
+function CorrectOrNot(answer, i){
+    
+    score = document.getElementById("score")
+    
+    if (answer === i){
+        scorenum ++;
+    }
+    score.innerHTML = `Score: ${scorenum}`
+
+}

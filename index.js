c47e8b217670761a771bf2a92820f4f6b5a748fc
Numsik <honzik.klusek.myto@gmail.com>
2026-05-25 19:46:37 +0200

    trochu design

--- index.js
+++ index.js
@@ -26,13 +26,32 @@ function showQuestion(index) {
 
   document.getElementById("question").innerHTML = q.questions
   document.getElementById("answers").innerHTML = q.answer
-    .map((answer, i) => `<button type="button" id="btn"  onclick="getAnotherQuestion(${index}); CorrectOrNot(${q.correct},${i})">${answer}</button>`).join("")
+    .map((answer, i) => `<button type="button" id="btn"  onclick="getAnotherQuestion(${index}); CorrectOrNot(${q.correct},${i}, ${index})">${answer}</button>`).join("")
 }
 
 function getRandomNum(max){
     return Math.floor(Math.random() * max)
 }
 showQuestion(getRandomNum(questions.length))
+
+
+
+let scorenum = 0
+function CorrectOrNot(answer, i, index){
+    score = document.getElementById("score")
+    
+    if (answer === i){
+        scorenum++
+    }
+    if (index >= questions.length - 1){
+          document.querySelectorAll("button").forEach(btn => {
+        btn.disabled = true
+        })  
+    }
+
+    
+    score.innerHTML = `Score: ${scorenum}`
+}
 
 function getAnotherQuestion(index){
     const next = index + 1
@@ -44,14 +63,3 @@ function getAnotherQuestion(index){
 
 }
 
-let scorenum = 0
-function CorrectOrNot(answer, i){
-    
-    score = document.getElementById("score")
-    
-    if (answer === i){
-        scorenum ++;
-    }
-    score.innerHTML = `Score: ${scorenum}`
-
-}
